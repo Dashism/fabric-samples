6b5794af12b8da04f2a410d98231cc2c46692b49
DARENNE <benoit.darenne@capgemini.com>
2019-06-06 12:53:23 +0200

    firstfrontpage

--- hackathon/FRONTEND/Hackathon/src/app/profile/profile.component.ts
+++ hackathon/FRONTEND/Hackathon/src/app/profile/profile.component.ts
@@ -12,3 +12,29 @@ export class ProfileComponent implements OnInit {
   }
 
 }
+
+  // addTenant() {
+  //   const skill = new Skill();
+  //   // tenant.projectId = tenant.project.id;
+  //   this.agent.skillList.push(skill);
+  //   this.addTenantsForm();
+  // }
+
+  // addTenantsForm() {
+  //   const control = <FormArray>this.projectForm.controls['skillList'];
+  //   control.push(this.initTenants());
+  // }
+
+  // addSkillForm() {
+  //   const control = <FormArray>this.projectForm.controls['skillList'];
+  //   control.push(this.initTenants());
+  //   initTenants() {
+  //     return this.formBuilder.group({
+  //       'obsTenantId': ['', Validators.required],
+  //       'publicKey': ['', Validators.required],
+  //       'privateKey': ['', Validators.required],
+  //       'comment': ['']
+  //     });
+  //   }
+  // }
+}
